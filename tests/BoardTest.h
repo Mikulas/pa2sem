b5ddbe9d7d97626dd42f9c770fa79cac00c17620
Mikulas <rullaf@gmail.com>
2015-03-27 21:33:58 +0100

    update board test

--- tests/BoardTest.h
+++ tests/BoardTest.h
@@ -33,9 +33,28 @@ public:
 		assert(nullptr != b0.hit(Location(1, s0.length)),
 			"hit last ok");
 
-		Ship s1(2);
-		s1.setStartEnd(Location(4, 0), Location(4, 1));
-		assert(nullptr != b0.hit(Location(4, 1)),
+
+        Board b1;
+        Ship s1(5);
+		s1.setStartEnd(Location(0, 0), Location(0, 4));
+        b1.ships.push_back(s1);
+        Ship s2(4);
+		s2.setStartEnd(Location(1, 0), Location(1, 3));
+        b1.ships.push_back(s2);
+        Ship s3(3);
+		s3.setStartEnd(Location(2, 0), Location(2, 2));
+        b1.ships.push_back(s3);
+        Ship s4(3);
+		s4.setStartEnd(Location(3, 0), Location(3, 2));
+        b1.ships.push_back(s4);
+        Ship s5(2);
+		s5.setStartEnd(Location(4, 0), Location(4, 1));
+        b1.ships.push_back(s5);
+        assert(nullptr != b1.hit(Location(4, 0)),
+			"hit end ok");
+		assert(nullptr != b1.hit(Location(4, 1)),
+			"hit end ok");
+		assert(nullptr == b1.hit(Location(4, 2)),
 			"hit end ok");
 	}
 };
