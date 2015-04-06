40477312c90f01ddeadd1d4182586dd434013346
Mikulas <rullaf@gmail.com>
2015-04-06 16:44:28 +0200

    improve random ship placement

--- src/inc/Player/AI/AI.cpp
+++ src/inc/Player/AI/AI.cpp
@@ -5,14 +5,15 @@ vector<Ship> AIPlayer::placeShipsRandomly(vector<Ship> ships) {
 		while (true) {
 			Location start = Random::location();
 			Location end = Random::location(start, ship.length);
+
 			ship.setStartEnd(start, end);
 
-
+			for (auto &other : ships) {
+				if (&ship != &other && ship.overlaps(&other)) {
+					continue;
+				}
+			}
 			break;
-			// TODO reimpl here
-			//if (board->canPlace(&ship)) {
-			//	break;
-			//}
 		}
 	}
 
